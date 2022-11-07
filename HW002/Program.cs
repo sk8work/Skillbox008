6ffe6e74be8672b6fe34857f987db33ccb2a0f7e
sk8work <eshikhantsev@gmail.com>
2022-11-07 18:24:55 +0500

    Добавьте файлы проекта.

---
+++ HW002/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
